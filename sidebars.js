35e4457b4d7057ebf1154bd8e50322f3d95887a7
Markus Lund <markuslund92@gmail.com>
2022-11-15 14:26:00 +0100

    Change to single quotes to match linting rules

--- sidebars.js
+++ sidebars.js
@@ -2,9 +2,9 @@
 
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
-  iam: require("./docs/iam/sidebars.js"),
-  cnaas: require("./docs/cnaas/sidebars.js"),
-  datadeling: require("./docs/datadeling/sidebars.js"),
+  iam: require('./docs/iam/sidebars.js'),
+  cnaas: require('./docs/cnaas/sidebars.js'),
+  datadeling: require('./docs/datadeling/sidebars.js'),
 };
 
 module.exports = sidebars;
